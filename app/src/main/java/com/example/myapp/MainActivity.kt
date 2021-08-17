3a755bf18fd11ce1eeee579c3679963779414ae3
jwUm <87863185+umjiwoo@users.noreply.github.com>
2021-08-17 20:36:40 +0900

    지혜 코드랑 합침

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,15 +1,42 @@
 package com.example.myapp
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        with(supportFragmentManager.beginTransaction()) {
+            val fragment5 = Map()
+            replace(R.id.container, fragment5)
+            commit()
+        }
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.tab1 -> {
+                    with(supportFragmentManager.beginTransaction()) {
+                        val fragment5 = Map()
+                        replace(R.id.container, fragment5)
+                        commit()
+                    }
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.tab2 -> {
+                    with(supportFragmentManager.beginTransaction()) {
+                        val fragment4 = MainListFragment()
+                        replace(R.id.container, fragment4)
+                        commit()
+                    }
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+
+            }
+            return@setOnNavigationItemSelectedListener false
+        }
     }
 }
