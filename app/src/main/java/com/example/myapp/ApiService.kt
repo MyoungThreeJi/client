a16002f0e129f046acfa74d232afeb96b014a003
sia7997 <sia7997@naver.com>
2021-08-22 21:54:14 +0900

    [Fix] 맵+전체 연결완료

--- app/src/main/java/com/example/myapp/ApiService.kt
+++ app/src/main/java/com/example/myapp/ApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface ApiService {
     companion object{
-        public val API_URL = "http://7bbc29ba7e82.ngrok.io"
+        public val API_URL = "http://1903-218-154-215-121.ngrok.io"
     }
     @GET("v1/pad")
     fun get_pad(@Query("format") json:String): Call<List<padInfo>>
