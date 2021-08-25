85cef3afb84c2badae1d8883fb96e5c0f3d7c03e
jwUm <87863185+umjiwoo@users.noreply.github.com>
2021-08-25 22:19:23 +0900

    [Fix]리뷰 닉네임 지정& 데이터 클래스 필드 명으로 수정

--- app/src/main/java/com/example/myapp/reviewInfoAdapter.kt
+++ app/src/main/java/com/example/myapp/reviewInfoAdapter.kt
@@ -25,13 +25,13 @@ class reviewInfoAdapter : RecyclerView.Adapter<reviewInfoAdapter.ViewHolder>(){
     inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
         fun setItem(item:reviewInfo){
             itemView.profile.setImageResource(R.drawable.heart)
-            itemView.user_id.text=item.id
-            itemView.date.text=item.date
-            itemView.rat1.rating=item.r1!!
-            itemView.rat2.rating=item.r2!!
-            itemView.rat3.rating=item.r3!!
-            itemView.rat4.rating=item.r4!!
-            itemView.review_content.text=item.review
+            itemView.user_id.text="익명의 리뷰어"
+            itemView.date.text=item.created
+            itemView.rat1.rating=item.star1!!
+            itemView.rat2.rating=item.star2!!
+            itemView.rat3.rating=item.star3!!
+            itemView.rat4.rating=item.star4!!
+            itemView.review_content.text=item.content
         }
     }
 }
