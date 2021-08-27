6693f8a397afedaee2c72e31c62c0d02de825a04
jwUm <87863185+umjiwoo@users.noreply.github.com>
2021-08-27 17:50:16 +0900

    [Feat] get_detection()추가

--- app/src/main/java/com/example/myapp/ApiService.kt
+++ app/src/main/java/com/example/myapp/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.*
 
 interface ApiService {
     companion object{
-        public val API_URL = "http://7b8b-218-154-215-121.ngrok.io"
+        public val API_URL = "http://83d7-182-215-164-209.ngrok.io"
     }
     @GET("v1/pad")
     fun get_pad(@Query("format") json:String): Call<List<padInfo>>
@@ -17,8 +17,11 @@ interface ApiService {
     fun get_id(
             @Path("id") id:Int):Call<padlist>
 
+    @GET("v1/detectionInfo/")
+    fun get_detection(@Query("format") json:String):Call<ArrayList<detectioninfo>>
+
     @POST("v1/review/")
-    fun reg_review(@Body review:p_reviewInfo):Call<reviewInfo>
+    fun reg_review(@Body review:p_reviewInfo):Call<p_reviewInfo>
 
     @GET("v1/review/")
     fun get_review(@Query("format") json:String):Call<List<reviewInfo>>
