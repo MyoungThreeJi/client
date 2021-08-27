bbb2a2753e172483e7f9c599ab4773df30b4183a
jwUm <87863185+umjiwoo@users.noreply.github.com>
2021-08-28 04:31:41 +0900

    API_URL수정

--- app/src/main/java/com/example/myapp/ApiService.kt
+++ app/src/main/java/com/example/myapp/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.*
 
 interface ApiService {
     companion object{
-        public val API_URL = "http://a567-218-154-215-121.ngrok.io"
+        public val API_URL = "http://b144-182-215-164-209.ngrok.io"
     }
     @GET("v1/pad")
     fun get_pad(@Query("format") json:String): Call<List<padInfo>>
