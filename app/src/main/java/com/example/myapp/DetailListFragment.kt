6e57c5055a70bbdd63932a8bb936f68462a216d3
jwUm <87863185+umjiwoo@users.noreply.github.com>
2021-09-02 01:39:06 +0900

    [Feat]git-pull해도 합쳐지지 않은 곳 있어서 수동으로 합침

--- app/src/main/java/com/example/myapp/DetailListFragment.kt
+++ app/src/main/java/com/example/myapp/DetailListFragment.kt
@@ -44,24 +44,6 @@ class DetailListFragment(position: Int) : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        class URLtoBitmapTask() : AsyncTask<Void, Void, Bitmap>() {
-            //액티비티에서 설정해줌
-            lateinit var url: URL
-            override fun doInBackground(vararg params: Void?): Bitmap {
-                val bitmap = BitmapFactory.decodeStream(url.openStream())
-                return bitmap
-            }
-
-            override fun onPreExecute() {
-                super.onPreExecute()
-
-            }
-
-            override fun onPostExecute(result: Bitmap) {
-                super.onPostExecute(result)
-            }
-        }
-
 
         val bundle = arguments
         val po = bundle?.getInt("position")
@@ -73,14 +55,13 @@ class DetailListFragment(position: Int) : Fragment() {
         layoutManager.setStackFromEnd(true)
         recy.layoutManager = layoutManager
 
-        adapter = ListdetailAdapter()
-        recy.adapter = adapter
 
         var retrofit = Retrofit.Builder().baseUrl(ApiService.API_URL).addConverterFactory(
                 GsonConverterFactory.create()).build()
         var apiService = retrofit.create(ApiService::class.java)
 
         var tests = apiService.get_id(idpo)
+
         var reviews = apiService.get_review("json")
 
         tests.enqueue(object : Callback<padlist> {
@@ -88,17 +69,6 @@ class DetailListFragment(position: Int) : Fragment() {
                 if (response.isSuccessful) {
                     var mList = response.body()!!
                     detailname.setText(mList.name.toString());
-                    //detailimg.setImageDrawable(mList.image)
-                    /* var i= mList.image
-                     var n= i!!.indexOf("i")
-                     var re=i.toString().substring((n+3)!!)
-                     Log.e("Re",re)
-                     var image_task: URLtoBitmapTask = URLtoBitmapTask()
-                     image_task = URLtoBitmapTask().apply {
-                         url = URL(re)
-                     }
-                     var bitmap: Bitmap = image_task.execute().get()
-                     detailimg.setImageBitmap(bitmap)*/
 
                     Log.e("Re", mList.image!!)
                     Log.e("Re", mList.image!!.substring(ApiService.API_URL.length + 1))
@@ -132,19 +102,30 @@ class DetailListFragment(position: Int) : Fragment() {
         tests1.enqueue(object : Callback<ArrayList<detectioninfo>> {
             override fun onResponse(call: Call<ArrayList<detectioninfo>>, response: Response<ArrayList<detectioninfo>>) {
                 if (response.isSuccessful) {
-                    var mList = response.body()!!
+                    var mList2 = response.body()!!
                     //Log.e("detection", mList.get(0).pad.toString())
 
-                    for (i in mList.indices) {
-                        if ((mList.get(i).pad)!!.equals(idpo)) {
+                    for (i in mList2.indices) {
+                        if ((mList2.get(i).pad)!!.equals(idpo)) {
 
-                            detections.add(mList.get(i).detection!!)
+                            detections.add(mList2.get(i).detection!!)
                         }
-                        Log.e("success", detections.toString())
-                        detections.reverse()
                     }
+                    Log.e("success", detections.toString())
+                    detections.reverse()
+
+                    var d = mutableListOf<List_IngreItem>()
+
+                    for (i in names.indices) {
+                        var reallist = List_IngreItem(names.get(i), mins.get(i), maxs.get(i), averages.get(i), detections.get(i))
+                        d.add(reallist)
+                    }
+
+                    adapter = ListdetailAdapter(d)
+                    recy.adapter = adapter
                 }
             }
+
 
             override fun onFailure(call: Call<ArrayList<detectioninfo>>, t: Throwable) {
                 Log.e("D_tests", "OnFailuer+${t.message}")
@@ -181,10 +162,9 @@ class DetailListFragment(position: Int) : Fragment() {
             }
         })
 
-        adapter.items.add(List_IngreItem("핵산", 8, 8, 8))
-        adapter.items.add(List_IngreItem("그로포", 8, 8, 8))
-        adapter.notifyDataSetChanged()
-
+//        adapter.items.add(List_IngreItem("핵산", 8, 8, 8))
+//        adapter.items.add(List_IngreItem("그로포", 8, 8, 8))
+//        adapter.notifyDataSetChanged()
 
 
         gotodi.setOnClickListener {
