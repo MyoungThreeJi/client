97a09f39286a44f3a43c805c780ebfc1615b1ee7
sia7997 <sia7997@naver.com>
2021-09-07 18:44:35 +0900

    1.상세리스트:성분정보,랭킹추가 2.뒤로가기 3.검색 4.아이콘 변경

--- app/src/main/java/com/example/myapp/ApiService.kt
+++ app/src/main/java/com/example/myapp/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.*
 
 interface ApiService {
     companion object{
-        public val API_URL = "http://b6a0-218-154-215-121.ngrok.io"
+        public val API_URL = ""
     }
     @GET("v1/pad")
     fun get_pad(@Query("format") json:String): Call<List<padInfo>>
